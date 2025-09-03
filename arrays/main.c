5c4dba26370bb99146c3afa9f86cccd7fceb1361
kithulovali <kithulovalibin@gmail.com>
2025-09-03 19:34:28 +0300

    arrays exercise 1 and 2

--- arrays/main.c
+++ arrays/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 int main(){
+// automated accessing
+// exercise number 2
+int myarray[] = {1,2,3,4,5}; // my array
+int arr = sizeof(myarray)/ sizeof(myarray[0]); // accessing all at once
+for (int i=0 ; i< arr; i++){ // looping to access them
+    printf("%d\t", myarray[i]);
 
-printf("helloworld");
+}
+printf("\n"); // space between the automatic  and the manual
+separator();
+second();
 return 0;
 
+
+}
+// manual accessing
+// exercise number 1
+void second(){
+int myarray[] = {1,2,3,4,5};
+printf("\n");
+ // declaring a array
+printf("%d\n",myarray[0]);
+printf("%d\n",myarray[1]);
+printf("%d\n",myarray[2]);
+printf("%d\n",myarray[3]);
+printf("%d\n",myarray[4]);
+}
+
+void separator(){
+printf("accessing in a menuall way");
 
 }
